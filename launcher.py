81496c1b815ec906d6c184bbc5509c84e7c97a9a
lucaboschiero <luca.boschiero@studenti.unitn.it>
2025-01-29 17:07:48 +0100

    modifica creazione log

--- launcher.py
+++ launcher.py
@@ -13,7 +13,7 @@ attacks = "backdoor/labelflipping"
 epochs = 30
 total_clients = 40
 attacker_percentage = [10, 20, 30, 40, 50, 60, 70]
-labelflipping_percentage = [50, 30, 70]
+labelflipping_percentage = [25, 50, 75]
 dataset = "mnist"
 
 # Open a file for logging
