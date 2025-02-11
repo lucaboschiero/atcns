39aa03c7110bccf0da7ccafb3853fce4620452e4
mauromeneghello <mauromeneghello01@gmail.com>
2025-02-11 21:13:43 +0100

    modifiche cifar ml

--- launcher.py
+++ launcher.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 # Define launcher execution log path
-execution_log_path = "./logs/execution_log.txt"
+execution_log_path = "./logs/execution_log_cifar.txt"
 
 # Define script and argument sets
 script_path = "./main.py"
@@ -15,7 +15,7 @@ total_clients = 40
 attacker_percentage = [10, 20, 30, 40, 50, 60, 70]
 #attacker_percentage = [60, 70]
 labelflipping_percentage = [25, 50, 75]
-dataset = "mnist"
+dataset = "cifar"
 
 # Open a file for logging
 with open(execution_log_path, "a") as log_file:
